c8d7dec47ea2811412493876ade36040cb9042f3
Xu Olivier <olivierxu36@gmail.com>
2026-03-25 16:24:41 +0100

    commit

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const isVercel = process.env.VERCEL === '1';
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
 
 // Page cartes Pokémon
 app.get('/', (req, res) => {
